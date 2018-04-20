fde0156d2fda674679ba50253540319163d15616
Edward Rudoi <ed@MacBook-Pro-Edward.local>
2018-04-20 03:06:09 +0300

    home 3

--- src/com/sckeedoo/certification/Problem3.java
+++ src/com/sckeedoo/certification/Problem3.java
@@ -1,11 +1,21 @@
 package com.sckeedoo.certification;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Problem3 {
+    static int date = 87089709;
     /*
-         Write a Java method to count all vowels in a string.
-         Method should take a String as argument, and return an Integer;
+        Write a Java program to convert minutes into a number of years and days.
+        Test Data
+        Input the number of minutes: 3456789
+        Expected Output :
+        3456789 minutes is approximately 6 years and 210 days
      */
     public static void main(String[] args) {
+        int days = (date % (60*24*365))/(60*24);
+        int years =  date / (60*24*365);
 
+        System.out.println(date+" minutes is approximately "+years+" years and " + days + " days ");
     }
 }
