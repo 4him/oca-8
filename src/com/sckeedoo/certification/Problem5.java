5b71c8ec18875f7f911fdd1d2ef989a685ea4fc1
4him <edwrudoi@mail.ru>
2018-04-26 17:19:52 +0300

    Update Problem5.java

--- src/com/sckeedoo/certification/Problem5.java
+++ src/com/sckeedoo/certification/Problem5.java
@@ -3,13 +3,20 @@ package com.sckeedoo.certification;
 import sun.tools.java.BinaryClass;
 
 public class Problem5 {
-    static int a = 20468;
+    static int a = 31;
     /*
         Given a number N, find if it's prime or not (Google what prime number means, in case you don't know)
      */
     public static void main(String[] arg) {
 
-
-
+        while(a != 0){
+            if((a & 1) != 0) { // if division on 2 not equals 0
+                a/=10;
+            }else{
+                System.out.println("This is not prime number");
+                return;
+            }
+        }
+        System.out.println("This is prime number");
     }
 }
